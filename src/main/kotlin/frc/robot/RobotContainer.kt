27931e8f09df0305b349a19fb8002b38dfb2c0bd
Zavion Cyboron <z.cyboron07@gmail.com>
2025-10-24 13:43:52 -0600

    Feat: Add Pathplanner autoChooser to files

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,18 +1,41 @@
 package frc.robot
 
+import com.pathplanner.lib.auto.AutoBuilder
+import com.pathplanner.lib.config.PIDConstants
+import com.pathplanner.lib.auto.NamedCommands
+import com.pathplanner.lib.config.RobotConfig
+import com.pathplanner.lib.controllers.PPHolonomicDriveController
+import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj.Joystick
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.InstantCommand
 import subsystems.DriveSubsystem
 import subsystems.DriveSubsystem.mecanumDrive
+import subsystems.DriveSubsystem.driverelative
+import org.photonvision.PhotonCamera
+import subsystems.DriveSubsystem.pose
 
 object RobotContainer {
 
-    init{
+    private val stick: Joystick = Joystick(0)
+
+    private var autoChooser: SendableChooser<Command>? = null
+
+    val autonomousCommand: Command
+        get() {
+            return autoChooser?.selected ?: InstantCommand()
+        }
+
+    init {
         configureBindings()
+        cameraSettings()
     }
 
     //private val  controller: XboxController = XboxController(0)
-    private val stick: Joystick = Joystick(0)
+
 
     private fun configureBindings() {
 
@@ -20,5 +43,9 @@ object RobotContainer {
             DriveSubsystem.run {
                 mecanumDrive(stick.getY(), -stick.getX(), -stick.getZ())
             }
+    }
+
+    private fun cameraSettings() {
+
     }
 }
